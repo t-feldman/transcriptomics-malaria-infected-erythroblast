2d13453cc3dc34a592aeba73f20b6eca547d1267
t-feldman <76502273+t-feldman@users.noreply.github.com>
2023-08-12 19:32:28 -0400

    Add files via upload

--- Scripts/Preprocessing-and-alignment/01_MovingFiles.sh
+++ Scripts/Preprocessing-and-alignment/01_MovingFiles.sh
@@ -1,25 +1,33 @@
 ﻿#!/bin/bash
-# Purpose: Move fastq files into sample directories using sample sheet
 # Author: Tamar Feldman
+# Project: Feldman et al. Blood Adv. 2023
+# Purpose: Move fastq files into sample directories
 
-DATA_DIR=$1 # Where the data are currently stored
-NEW_PATH=$2 # Path to new parent directory
+# Requires the sample sheet file: 2021AUG06_SampleSheet.csv
+# The sample sheet associates fastq files with biological samples
+
+# Inputs
+DATA_DIR=$1 # Path to where the data are currently stored
+NEW_PATH=$2 # Path to dsetination parent directory
 SAMPLE_SHEET=$3 # Path to sample sheet
-# use compound function to skip headers with extra read
-# need extra variable to contain remaining columns
+
+# Skip the header row in the sample sheet and
+# use the sample sheet to get the file name and destination.
+# Copy the file to the destination directory.
 {
 	read
 	while IFS=, read -r FLNAME NEW_DIR EXTRA;
 	do
 		echo "Name : $FLNAME"
 		echo "Directory name: $NEW_DIR"
-		# remove any non-alnum or punct
-
+		
+		# Clean up the file name extracted from the sample sheet
+		# by removing anything that is not alphanumeric or punctuation
 		CLEAN_FL=${FLNAME//[^[:graph:]]/}
 		CLEAN_ND=${NEW_DIR//[^[:graph:]]/}
 
 
-		# check if directory exists & make if needed
+		# check if destination directory exists & make one if needed
 		[[ -d "$NEW_PATH/$NEW_DIR" ]] || mkdir -p "$NEW_PATH/$NEW_DIR"
 
 		#if the file exists, copy file to new directory
